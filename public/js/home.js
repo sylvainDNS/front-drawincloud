15daf0db2bd9afa52b16b0751a0ad4d211f3e42a
sylvainDNS <sylvain.denyse@gmail.com>
2018-02-09 15:12:00 +0100

    home page

---
+++ public/js/home.js
@@ -0,0 +1,12 @@
+(function () {
+    const process = {
+        env: {
+            IP: '127.0.0.1:4444'
+        }
+    }
+
+    // const req = 'http://'
+    // $.ajax({
+    // })
+
+}).call(this);
