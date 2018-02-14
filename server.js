b9c8ca496a515219b8687ad924055e427cebb90f
sylvainDNS <sylvain.denyse@gmail.com>
2018-02-14 17:16:35 +0100

    implement homepage draft

--- server.js
+++ server.js
@@ -18,11 +18,14 @@ const checkError = (error, body) => {
 }
 
 app.get('/', (req, res) => {
-  request(process.env.IP + '/snapshot', (error, response, body) => {
-    const data = checkError(error, body)
-    res.render('index.html', {
-      data: data
-    })
+  request.get(process.env.IP + '/snapshot', (error, response, body) => {
+    if (error) {
+      res.send(error.code)
+    } else {
+      res.render('index.html', {
+        data: body
+      })
+    }
   })
 })
 
@@ -30,6 +33,21 @@ app.get('/draw', (req, res) => {
   res.render('draw.html')
 })
 
+app.get('/draw/:id', (req, res) => {
+  const id = req.params.id
+  request.get(process.env.IP + '/snapshot/' + id, (error, response, body) => {
+    if (error) {
+      res.send(error.code)
+    } else {
+      const data = JSON.parse(body)
+      res.render('draw.html', {
+        dataURL: data.dataURL,
+        title: data.title
+      })
+    }
+  })
+})
+
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
+  console.log('Front listening on port 3000')
 })
